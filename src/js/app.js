c5d9720dd294187ec02abc51cd924e5e68838844
iqbol <iqbol@localhost>
2020-01-21 17:50:06 +0300

    Some fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
-const root = document.querySelector('[data-id=root]');
-root.innerHTML=
-`
+const root = document.querySelector("[data-id=root]");
+root.innerHTML = `
 <form>
 <label for="sum">SUM:</label>
 <input id="sum" data-id="sum" placeholder="sum"></input>
@@ -13,83 +12,76 @@ root.innerHTML=
 </ul>
 <div id="total" data-id="final-sum"></div>
 `;
-const addButton = document.querySelector('[data-id=add-button]');
-const inputSum = document.querySelector('[data-id=sum]');
-const inputCategory = document.querySelector('[data-id=category]');
-const finalSum = document.querySelector('[data-id=final-sum]');
-const sumList = document.querySelector('[data-id=sum-list]');
-const message = document.querySelector('[data-id=message]');
-message.style.display='none';
+const addButton = document.querySelector("[data-id=add-button]");
+const inputSum = document.querySelector("[data-id=sum]");
+const inputCategory = document.querySelector("[data-id=category]");
+const finalSum = document.querySelector("[data-id=final-sum]");
+const sumList = document.querySelector("[data-id=sum-list]");
+const message = document.querySelector("[data-id=message]");
+message.style.display = "none";
 let amaunt = 0;
-addButton.addEventListener('click',(evt)=>
-{
-    evt.preventDefault();    
-     if (!isNaN(parseInt(inputSum.value))) {
-        message.style.display='none';
-        finalSum.textContent=`Total : ${ amaunt +=parseInt(inputSum.value)} `;   
-        const item =document.createElement('li');
-        item.innerHTML = `sum: ${inputSum.value}  -  category: ${inputCategory.value} `;
-        const delbutton = document.createElement('button');
-        delbutton.id='button-delete';
-        const upButton = document.createElement('button');
-        upButton.id='button-up';
-        const downButton = document.createElement('button');
-        downButton.id='button-down';
-        upButton.textContent='↑';
-        downButton.textContent='↓';
-        delbutton.textContent=`x`;
-        delbutton.addEventListener('click',()=>
-        {
-            item.remove();
-            var num = parseInt(item.innerHTML.replace(/\D+/g,""));      
-             finalSum.textContent =`Total : ${amaunt-= num}`;
-            
-        });
-        upButton.addEventListener('click',()=>{
-            let indexer=0;
-           for (let i = 0; i < sumList.childNodes.length; i++) {
-                 if (sumList.childNodes[i]===item) {              
-                        indexer=i;                                           
-                }
-           }
-           if (upButton.parentElement!=sumList.firstElementChild) {
-           sumList.insertBefore(item,sumList.childNodes[indexer].previousElementSibling );           
-                }
-                else{
-                    sumList.insertBefore(item,sumList.lastElementChild);
-                }
-            }       
+addButton.addEventListener("click", evt => {
+  evt.preventDefault();
+  if (!isNaN(parseInt(inputSum.value))) {
+    message.style.display = "none";
+    finalSum.textContent = `Total : ${(amaunt += parseInt(inputSum.value))} `;
+    const item = document.createElement("li");
+    item.innerHTML = `sum: ${inputSum.value}  -  category: ${inputCategory.value} `;
+    const delbutton = document.createElement("button");
+    delbutton.id = "button-delete";
+    const upButton = document.createElement("button");
+    upButton.id = "button-up";
+    const downButton = document.createElement("button");
+    downButton.id = "button-down";
+    upButton.textContent = "↑";
+    downButton.textContent = "↓";
+    delbutton.textContent = `x`;
+    delbutton.addEventListener("click", () => {
+      item.remove();
+      var num = parseInt(item.innerHTML.replace(/\D+/g, ""));
+      finalSum.textContent = `Total : ${(amaunt -= num)}`;
+    });
+    upButton.addEventListener("click", () => {
+      let indexer = 0;
+      for (let i = 0; i < sumList.childNodes.length; i++) {
+        if (sumList.childNodes[i] === item) {
+          indexer = i;
+        }
+      }
+      if (upButton.parentElement != sumList.firstElementChild) {
+        sumList.insertBefore(
+          item,
+          sumList.childNodes[indexer].previousElementSibling
         );
-        downButton.addEventListener('click',()=>
-        {
-            let indexer=0;
-            for (let i = 0; i < sumList.childNodes.length; i++) {
-                  if (sumList.childNodes[i]===item) {              
-                         indexer=i;                                           
-                 }
-            }
-               if (downButton.parentElement!=sumList.lastElementChild) {
-                sumList.insertBefore(item,sumList.childNodes[indexer+1].nextElementSibling );               
-               } else
-               {
-                   sumList.insertBefore(item,sumList.firstElementChild);
-               }           
-        });
-        item.appendChild(delbutton);
-        item.appendChild(upButton);
-        item.appendChild(downButton);
-        sumList.insertBefore(item,sumList.firstElementChild);
-        inputSum.value = '';     
-        inputCategory.value = '';
-         
-     }else
-     {
-        message.style.display = 'block';
-        inputSum.value = '';     
-        inputCategory.value = '';
-     }
-     
-   
+      } else {
+        sumList.insertBefore(item, sumList.lastElementChild);
+      }
+    });
+    downButton.addEventListener("click", () => {
+      let indexer = 0;
+      for (let i = 0; i < sumList.childNodes.length; i++) {
+        if (sumList.childNodes[i] === item) {
+          indexer = i;
+        }
+      }
+      if (downButton.parentElement != sumList.lastElementChild) {
+        sumList.insertBefore(
+          item,
+          sumList.childNodes[indexer + 1].nextElementSibling
+        );
+      } else {
+        sumList.insertBefore(item, sumList.firstElementChild);
+      }
+    });
+    item.appendChild(delbutton);
+    item.appendChild(upButton);
+    item.appendChild(downButton);
+    sumList.insertBefore(item, sumList.firstElementChild);
+    inputSum.value = "";
+    inputCategory.value = "";
+  } else {
+    message.style.display = "block";
+    inputSum.value = "";
+    inputCategory.value = "";
+  }
 });
-
- 
