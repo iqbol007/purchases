b1176d5f679a058b9220426224faf88e865f015b
iqbol <iqbol@localhost>
2020-01-27 10:27:20 +0300

    Some fixes

--- js/app.js
+++ js/app.js
@@ -1,32 +1,53 @@
+import {Purchase} from "./lib.js"
+
 const root = document.querySelector("[data-id=root]");
+
 root.innerHTML = `
-<form>
-<label for="sum">SUM:</label>
-<input id="sum" data-id="sum" placeholder="sum"></input>
-<label for="category">CTEGORY:</label>
-<input id="category" data-id="category" placeholder="category"></input>
-<button id="add-button" data-id="add-button">Add purchase</button>
-</form>
-<span data-id="message" id="message">Amount entered incorrectly!</span>
-<ul id="sum-list" data-id="sum-list">
-</ul>
-<div id="total" data-id="final-sum"></div>
+<form> 
+   <label for="sum">SUM:</label> 
+   <input id="sum" data-id="sum" placeholder="sum"></input> 
+   <label for="category">CTEGORY:</label>
+<select id="category" data-id="category" placeholder="category">
+<option>Food</option>
+<option>Travel</option>
+<option>Beauty</option>
+<option>Cars</option>
+</select>
+   <button id="add-button" data-id="add-button">Add purchase</button> 
+ </form> 
+ <span data-id="message" id="message">Amount entered incorrectly!</span> 
+ <button data-id="no-sort">No sort</button>
+ <button data-id="sort-by-price-up"> Sort expensive</button>
+ <button data-id="sort-by-price-down">Sort chip</button>
+ <ul id="sum-list" data-id="sum-list"></ul> 
+ <div id="total" data-id="final-sum"></div> 
 `;
+
 const addButton = document.querySelector("[data-id=add-button]");
 const inputSum = document.querySelector("[data-id=sum]");
 const inputCategory = document.querySelector("[data-id=category]");
+
 const finalSum = document.querySelector("[data-id=final-sum]");
 const sumList = document.querySelector("[data-id=sum-list]");
 const message = document.querySelector("[data-id=message]");
+
 message.style.display = "none";
 let amaunt = 0;
+
 addButton.addEventListener("click", evt => {
   evt.preventDefault();
-  if (!isNaN(parseInt(inputSum.value))) {
-    message.style.display = "none";
-    finalSum.textContent = `Total : ${(amaunt += parseInt(inputSum.value))} `;
+  const value = parseInt(inputSum.value,10);
+  if (!isNaN(value)) {
+    const category = inputCategory.value;
+    const purchase =new Purchase(value,category);
     const item = document.createElement("li");
-    item.innerHTML = `sum: ${inputSum.value}  -  category: ${inputCategory.value} `;
+
+    message.style.display = "none";
+    finalSum.textContent = `Total : ${(amaunt +=value)} `;
+
+    item._purchase=purchase;
+    item.innerHTML = `sum: ${value}  -  category: ${inputCategory.value} `;
+
     const delbutton = document.createElement("button");
     delbutton.id = "button-delete";
     const upButton = document.createElement("button");
@@ -35,44 +56,34 @@ addButton.addEventListener("click", evt => {
     downButton.id = "button-down";
     upButton.textContent = "↑";
     downButton.textContent = "↓";
-    delbutton.textContent = `x`;
+    delbutton.textContent = 'x';
+
     delbutton.addEventListener("click", () => {
       item.remove();
       var num = parseInt(item.innerHTML.replace(/\D+/g, ""));
       finalSum.textContent = `Total : ${(amaunt -= num)}`;
     });
-    upButton.addEventListener("click", () => {
-      let indexer = 0;
-      for (let i = 0; i < sumList.childNodes.length; i++) {
-        if (sumList.childNodes[i] === item) {
-          indexer = i;
-        }
-      }
-      if (upButton.parentElement != sumList.firstElementChild) {
+
+    upButton.addEventListener("click", () => {    
+     if (upButton.parentElement != sumList.firstElementChild) {
+        
         sumList.insertBefore(
           item,
-          sumList.childNodes[indexer].previousElementSibling
+          item.previousElementSibling
         );
-      } else {
-        sumList.insertBefore(item, sumList.lastElementChild);
+      } else if (item.previousElementSibling==null) {
+        sumList.appendChild(item);
       }
     });
+
     downButton.addEventListener("click", () => {
-      let indexer = 0;
-      for (let i = 0; i < sumList.childNodes.length; i++) {
-        if (sumList.childNodes[i] === item) {
-          indexer = i;
-        }
-      }
       if (downButton.parentElement != sumList.lastElementChild) {
-        sumList.insertBefore(
-          item,
-          sumList.childNodes[indexer + 1].nextElementSibling
-        );
-      } else {
-        sumList.insertBefore(item, sumList.firstElementChild);
-      }
+            sumList.insertBefore(item.nextElementSibling,item);
+       } else if(item.nextElementSibling ==null) {
+      sumList.insertBefore(item, sumList.firstElementChild);
+       }
     });
+
     item.appendChild(delbutton);
     item.appendChild(upButton);
     item.appendChild(downButton);
@@ -85,3 +96,30 @@ addButton.addEventListener("click", evt => {
     inputCategory.value = "";
   }
 });
+
+const buttonNoSort = document.querySelector('[data-id=no-sort]');
+const buttonUpPrice = document.querySelector('[data-id=sort-by-price-up]');
+const buttonDownPrice = document.querySelector('[data-id=sort-by-price-down]');
+
+buttonNoSort.onclick = ()=>
+{
+
+}
+
+buttonUpPrice.onclick = ()=>
+{
+  const purchases = Array.from(sumList.children);
+  purchases.sort((a,b)=>-(a._purchase.amount-b._purchase.amount))
+  for (const purchase of purchases) {
+    sumList.appendChild(purchase);
+  }
+}
+
+buttonDownPrice.onclick = ()=>
+{
+  const purchases = Array.from(sumList.children);
+  purchases.sort((a,b)=> a._purchase.amount-b._purchase.amount);
+  for (const purchase of purchases) {
+    sumList.appendChild(purchase);
+  }
+}
